e31acd41fbb96346d74b100dbcf5476da9a98669
srSenhor <viniciusfeliciano14@gmail.com>
2023-01-27 22:24:41 -0300

    Algumas questões

---
+++ ed1/src/list1/q1.java
@@ -0,0 +1,18 @@
+package list1;
+
+public class q1 
+{
+	private static int my_age = 19;
+	private static float my_height = 1.68f;
+	private static char first_char = 'V';
+	private static String my_name = "Vinicius Feliciano Alves do Nascimento";
+	
+	public static void main(String args[])
+	{
+		System.out.println("Nome completo: " + my_name);
+		System.out.println("Primeira letra do nome: " + first_char);
+		System.out.println("Idade: " + my_age);
+		System.out.println("Altura: " + my_height);
+	}
+	
+}
